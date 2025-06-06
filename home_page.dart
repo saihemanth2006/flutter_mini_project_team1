70a3dc05c228d383831d1daf8094a92a95f19d2d
saihemanth2006 <23A91A1235@aec.edu.in>
2025-06-06 19:21:51 +0530

    Update home_page.dart

--- home_page.dart
+++ home_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'profile_card.dart';
 //welcome page
 class HomeScreen extends StatelessWidget{
-  //const HomeScreen({super.key});
-  @override   
+  const HomeScreen({super.key});
+  @override
   Widget build(BuildContext context) {
    return Scaffold(
      backgroundColor: Colors.black,
@@ -40,23 +40,23 @@ class HomeScreen extends StatelessWidget{
 
                //aravind profile container
                SizedBox(width: 20,),
-               ProfileCard(imageUrl: 'assets/aravind.jpg', name: 'P. D. S. Aravind Kumar', rollNo: '23MH1A1252', colors: Colors.amber,),
+               ProfileCard(imageUrl: 'assets/aravind.jpeg', name: 'P. D. S. Aravind Kumar', rollNo: '23MH1A1252', colors: Colors.amber,pno:'2893573423',loc:'Surampalem'),
 
                //Hemanth 1 profile container
                SizedBox(width: 20,),
-               ProfileCard(imageUrl: 'assets/me.jpg', name: 'M. V. S. Hemanth', rollNo: '23A91A1235', colors: Colors.blue.shade300),
+               ProfileCard(imageUrl: 'assets/me.jpg', name: 'M. V. S. Hemanth', rollNo: '23A91A1235', colors: Colors.blue.shade300,pno:'9845711344',loc:'Tunu'),
 
                 //nanada profile container
                SizedBox(width: 20,),
-               ProfileCard(imageUrl: 'assets/nanda.jpg', name: 'T. D. Nanda Kishore', rollNo: '23A91A12C5', colors: Colors.blueGrey),
+               ProfileCard(imageUrl: 'assets/nanda.jpg', name: 'T. D. Nanda Kishore', rollNo: '23A91A12C5', colors: Colors.blueGrey,pno:'8457214528',loc:'Kakinada'),
                SizedBox(width: 20,),
 
                //shalem profile container
-               ProfileCard(imageUrl: 'assets/syam.jpg', name: 'A. Shalem', rollNo: '23MH1A42D3', colors: Colors.greenAccent.shade200),
+               ProfileCard(imageUrl: 'assets/syam.jpg', name: 'A. Shalem', rollNo: '23MH1A42D3', colors: Colors.greenAccent.shade200,pno:'9457384648',loc:'Rajamhundry'),
 
                 //hemanth 2 conatiner
                SizedBox(width: 20,),
-               ProfileCard(imageUrl: 'assets/hemanth.jpg', name: 'N. Hemanth', rollNo: '23A91A12A6', colors: Colors.deepOrange.shade300),
+               ProfileCard(imageUrl: 'assets/hemanth.jpg', name: 'N. Hemanth', rollNo: '23A91A12A6', colors: Colors.deepOrange.shade300,pno:'9124738344',loc:'Srikakulam'),
                SizedBox(width: 20,),
 
              ],
